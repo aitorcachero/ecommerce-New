8ababd0208edd723fd2881e7c1e6fcaed34118e0
aicadev <aitor.cachero@gmail.com>
2025-06-16 10:50:57 +0200

    feat(products): increase product fetch limit from 100 to 194
    The limit was increased to fetch more products from the API in a single request, reducing the need for pagination or multiple requests.

--- .history/src/services/products/getAllProducts_20240122205658.js
+++ .history/src/services/products/getAllProducts_20240122205658.js
@@ -1,5 +1,5 @@
 async function getAllProducts() {
-  const data = await fetch('https://dummyjson.com/products/?limit=194');
+  const data = await fetch('https://dummyjson.com/products/?limit=100');
   const json = await data.json();
 
   return json.products;
